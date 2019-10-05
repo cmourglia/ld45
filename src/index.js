1d1c27ef1cb35213649e666cb39bb7b0841f0b4d
mytho123 <mytho123@gmail.com>
2019-10-05 12:36:35 +0200

    scale

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import 'phaser'
 import { Title } from './scenes/title.js';
 import { SimpleScene } from './scenes/simple-scene';
 
-const gameConfig = {
-	width: 680,
-	height: 400,
+new Phaser.Game({
+	width: 3600,
+	height: 3600,
+	zoom: ".25",
 	scene: [Title]
-};
-
-new Phaser.Game(gameConfig);
+});
