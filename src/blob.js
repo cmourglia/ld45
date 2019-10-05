50fe3e08fbfe0a9ddb0d9c612bf8dc2319752840
VincentPonchaut <vincent.ponchaut@hotmail.fr>
2019-10-05 17:11:49 +0200

    camembert

--- src/blob.js
+++ src/blob.js
@@ -3,39 +3,54 @@ import Phaser from 'phaser';
 const { Body } = Phaser.Physics.Matter.Matter;
 
 class Shape {
-    constructor(scene, color) {
-        this.scene = scene;
-        this.color = color;
+    constructor(graphics, color) {
+        this.graphics = graphics
+        this.color = color
     }
-
-    drawAtPos(x, y) {
-        console.log("drawAtPos");
+    drawOn(graphics) {
+        throw "RemiCroutel";
+        console.log("drawOn");
     }
 }
 
 class Arc extends Shape {
-    constructor(scene, color, radius) {
-        super(scene, color);
-        this.radius = radius;
+    constructor(graphics, color, radius) {
+        super(graphics, color)
+        this.radius = radius
     }
+    drawOn(graphics) {
+        graphics.moveTo(0, 0);
+        graphics.beginPath();
+        graphics.lineStyle(0, this.color);
+        graphics.fillStyle(this.color, 1.0);
 
-    drawAtPos(x, y) {
-        this.graphics = this.scene.add.graphics({ x, y });
-        this.graphics.beginPath();
-        this.graphics.fillStyle(this.color, 1.0);
-        this.graphics.arc(this.x + this.radius, this.y + this.radius, this.radius, 0, 2 * Math.PI);
-        this.graphics.fillPath();
+        // First line
+        graphics.lineBetween(0, 0, 0, - this.radius)
+
+        // Second line
+        graphics.lineBetween(0, 0, this.radius, 0)
+
+        // Arc
+        graphics.arc(0, 0, this.radius, 0, -0.5 * Math.PI, true);
+        graphics.fillPath();
     }
 }
 
 export class Quadrant {
-    constructor(shape) {
-        this.shape = shape;
-        this.subQuadrants = [];
+    constructor(scene, shape) {
+        this.scene = scene
+        this.shape = shape
+        this.subQuadrants = []
     }
 
     addQuadrant(shape) {
         this.subQuadrants.push(new Quadrant(shape));
+    }
+
+    draw() {
+        this.gameObject = this.scene.add.graphics()
+        this.shape.drawOn(this.gameObject)
+        //graphics.rotateCanvas(45 * degToRad + (this.quadrantIndex + 1) * 90 * degToRad)
     }
 
     traverse(callback) {
@@ -47,7 +62,19 @@ export class Quadrant {
 class Blob extends Phaser.GameObjects.GameObject {
     constructor(scene) {
         super(scene);
-        this.scene = scene;
+        this.scene = scene
+        this.quadrants = []
+
+        // By default: 4 blue circles with radius 30
+        let colors = [
+            0xFF0000,
+            0x00FF00,
+            0xFF00FF,
+            0xFFFF00
+        ]
+        for (let i = 0; i < 4; ++i) {
+            this.quadrants.push(new Quadrant(this.scene, new Arc(this.scene, colors[i], 30)))
+        }
     }
 
     generateGeometry(radius = 20, color = 0xFF0000, lineColor = 0x000000) {
@@ -58,15 +85,37 @@ class Blob extends Phaser.GameObjects.GameObject {
         // Create the graphics object
         this.graphics = this.scene.add.graphics({ x: 0, y: 0 });
 
-        this.graphics.lineStyle(2, lineColor, 1);
+        // Draw the quadrants
+        this.quadrants.forEach((q, i) => {
+            q.draw()
+            q.gameObject.setRotation((i * 90 - 45) * degToRad)
+        })
 
-        // Add a physic body to the graphics
-        this.graphics.fillStyle(color, 1.0);
-        this.graphics.beginPath();
+        this.graphics = this.scene.add.container(0, 0, this.quadrants.map(x => x.gameObject))
 
-        this.graphics.arc(0, 0, radius, 0, 2 * Math.PI);
-        this.graphics.fillPath();
-        this.graphics.strokePath();
+        // this.graphics = this.scene.add.circle(0, 0, 10, 0xFF0000)
+
+        // // Store params as members
+        // this.x = x
+        // this.y = y
+        // this.color = color
+        // this.lineColor = lineColor
+
+        // // Create the graphics object
+        // this.graphics = this.scene.add.graphics({
+        //     x: this.x,
+        //     y: this.y
+        // })
+
+        // this.graphics.lineStyle(2, lineColor, 1);
+
+        // // Add a physic body to the graphics
+        // this.graphics.fillStyle(color, 1.0);
+        // this.graphics.beginPath();
+
+        // this.graphics.arc(0, 0, radius, 0, 2 * Math.PI);
+        // this.graphics.fillPath();
+        // this.graphics.strokePath();
 
         // Add a physic body to the graphics
         const matterEnabledContainer = this.scene.matter.add.gameObject(this.graphics);
@@ -97,3 +146,5 @@ class Blob extends Phaser.GameObjects.GameObject {
 }
 
 export default Blob;
+
+const degToRad = Math.PI / 180.0
