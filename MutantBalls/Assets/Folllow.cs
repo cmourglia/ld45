a01099fc176c58272ef46251e092ca72447b378f
mytho123 <mytho123@gmail.com>
2019-10-06 15:36:29 +0200

    add target

---
+++ MutantBalls/Assets/Folllow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Folllow : MonoBehaviour
+{
+    public GameObject Target;
+
+    void Update()
+    {
+        this.transform.position = this.Target.transform.position;
+    }
+}
---
+++ MutantBalls/Assets/MateSelector.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MateSelector : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject TargetAim;
+    private Collider2D playerCollider;
+    private Collider2D collider;
+    private HashSet<Collider2D> triggers = new HashSet<Collider2D>();
+
+    void Start()
+    {
+        this.collider = this.GetComponent<Collider2D>();
+        this.playerCollider = this.Player.GetComponent<Collider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other == playerCollider) { return; }
+        this.triggers.Add(other);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        this.triggers.Remove(other);
+    }
+
+    void Update()
+    {
+        var target = this.triggers.OrderBy(other => (this.transform.position - other.transform.position).sqrMagnitude).FirstOrDefault();
+        if (target == null)
+        {
+            this.TargetAim.SetActive(false);
+        }
+        else
+        {
+            this.TargetAim.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, this.TargetAim.transform.position.z);
+            this.TargetAim.SetActive(true);
+        }
+    }
+}
