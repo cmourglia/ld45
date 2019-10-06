a9af82cd57d10c6cff1cc1a6e734eb431ff31e14
mytho123 <mytho123@gmail.com>
2019-10-06 14:57:12 +0200

    random colors & size

---
+++ MutantBalls/Assets/Blob.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blob : MonoBehaviour
+{
+    public float HP;
+    public float Size;
+    public Color Color;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        this.Color = Random.ColorHSV(0, 1, .5f, 1, .5f, 1);
+        this.Size = 1 + Random.value * 4;
+        this.HP = this.Size;
+
+        this.spriteRenderer = this.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.spriteRenderer.color = this.Color;
+        this.transform.localScale = new Vector3(this.Size, this.Size, 0);
+    }
+}
