5957eab0469a1f951b99c876f4c644e0158fe26a
mytho123 <mytho123@gmail.com>
2019-10-06 13:26:33 +0200

    player

---
+++ MutantBalls/Assets/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public float jump;
+    public GameObject rayOrigin;
+    public float rayCheckDistance;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        rb.velocity = new Vector3(x * speed, y * speed, 0);
+
+    }
+}
