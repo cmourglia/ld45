4686ae2ac7fab2eacfa0d3bb7fb1c9a3280434ec
mytho123 <mytho123@gmail.com>
2019-10-06 16:38:46 +0200

    close popup

--- MutantBalls/Assets/CopulatePopup.cs
+++ MutantBalls/Assets/CopulatePopup.cs
@@ -8,22 +8,19 @@ public class CopulatePopup : MonoBehaviour
     public Blob Parent2;
     public Blob Child;
 
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void Show(Blob parent1, Blob parent2)
     {
         this.gameObject.SetActive(true);
         parent1.CopyTo(this.Parent1);
         parent2.CopyTo(this.Parent2);
         this.Child.MergeFrom(this.Parent1, this.Parent2);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            this.gameObject.SetActive(false);
+        }
     }
 }
