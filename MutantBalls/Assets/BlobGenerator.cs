e0eb0af7c686ad53a00b2726f8233ab3e00ffec5
mytho123 <mytho123@gmail.com>
2019-10-06 14:41:04 +0200

    add blob generator

---
+++ MutantBalls/Assets/BlobGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobGenerator : MonoBehaviour
+{
+    public int nbBlobs = 100;
+    public Bounds area = new Bounds(new Vector3(0, 0, 0), new Vector3(5, 5, 0));
+    public GameObject BlobPrefab;
+
+
+    void Start()
+    {
+        Debug.Log(area.min);
+        for (var i = 0; i < nbBlobs; i++)
+        {
+            Instantiate(this.BlobPrefab, new Vector3(Random.Range(area.min.x, area.max.x), Random.Range(area.min.y, area.max.y), 0), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
