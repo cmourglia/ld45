07481f57858f28663d734ec90409828a16300863
VincentPonchaut <vincent.ponchaut@hotmail.fr>
2019-10-07 16:03:24 +0200

    hitting yourself no longer damage you

--- MutantBalls/Assets/HurtBlobs.cs
+++ MutantBalls/Assets/HurtBlobs.cs
@@ -7,12 +7,60 @@ public class HurtBlobs : MonoBehaviour
     public int Damage = 1;
     public bool Enabled;
 
+    public bool isHingedJointWith(Blob theBlob,GameObject root)
+    {
+        var hjs = root.GetComponents<HingeJoint2D>();
+        foreach (HingeJoint2D hj in hjs)
+        {
+            var b = hj.connectedBody.gameObject.GetComponent<Blob>();
+            if (b != null && b == theBlob)
+            {
+                return true;
+            }
+            var resultInJointedBodies = isHingedJointWith(theBlob, hj.connectedBody.gameObject);
+            if (resultInJointedBodies)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private bool _findThisInConnectedBodies(GameObject source)
+    {
+        var hjs = source.GetComponents<HingeJoint2D>();
+        foreach (HingeJoint2D hj in hjs)
+        {
+            var cg = hj.connectedBody.gameObject;
+            if (cg == this.gameObject)
+            {
+                return true;
+            }
+            var connected = _findThisInConnectedBodies(cg);
+            if (connected)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool belongsTo(Blob b)
+    {
+        return _findThisInConnectedBodies(b.gameObject);
+    }
+
     void OnCollisionEnter2D(Collision2D other)
     {
         if (!this.Enabled) { return; }
 
         var b = other.gameObject.GetComponent<Blob>();
-        if (b != null && b.gameObject != this.gameObject)
+
+        if (b == null || this.belongsTo(b))
+            return;
+
+        if (b.gameObject != this.gameObject)
         {
             b.Hurt(this.Damage);
         }
